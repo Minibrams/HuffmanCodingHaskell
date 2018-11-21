dbeeb6187d59cadf48640d976fbe5e712c345893
minibrams <anders@brams.dk>
2018-11-21 02:37:36 +0100

    Added support for n-ary Huffman tree encoding and decoding

--- huffman2.hs
+++ huffman2.hs
@@ -1,26 +1,38 @@
 import Data.List
 
 -- Data types 
-data Tree = Leaf Char Integer | Node Tree Tree Integer deriving Show
-data Bit  = One | Zero deriving (Show, Eq)
+data Tree = Leaf Char Int | Node [Tree] Int deriving Show
 
 -- Defining instances of Eq and Ord so I can compare Nodes and Leaves with each other based on their frequency
 instance Eq Tree where 
     (Leaf _ f1)   == (Leaf _ f2)   = f1 == f2 
-    (Node _ _ f1) == (Node _ _ f2) = f1 == f2 
+    (Node _ f1) == (Node _ f2) = f1 == f2 
 
 instance Ord Tree where 
     compare (Leaf _ f1)   (Leaf _ f2)   = compare f1 f2 
-    compare (Node _ _ f1) (Node _ _ f2) = compare f1 f2 
-    compare (Node _ _ f1) (Leaf _ f2)   = compare f1 f2 
-    compare (Leaf _ f1)   (Node _ _ f2) = compare f1 f2 
+    compare (Node _ f1) (Node _ f2) = compare f1 f2 
+    compare (Node _ f1) (Leaf _ f2)   = compare f1 f2 
+    compare (Leaf _ f1)   (Node _ f2) = compare f1 f2 
 
 -- Utility functions 
 key (k, v) = k 
 value (k, v)= v
 frequency (Leaf _ f) = f 
-frequency (Node _ _ f) = f 
+frequency (Node _ f) = f 
 sum_frequency x y = (frequency x) + (frequency y)
+
+set_minus lst1 lst2 = set_minus_helper lst1 lst2 []
+set_minus_helper [] lst acc = acc 
+set_minus_helper lst [] acc = acc ++ lst 
+set_minus_helper (x:xs) (y:ys) acc = 
+    if x == y then set_minus_helper xs ys acc 
+    else set_minus_helper xs ys (acc ++ [x])
+
+sum_frequencies [] = 0
+sum_frequencies lst =
+    let frequencies = [f | f <- (map (\x -> frequency x) lst) ] in
+        sum frequencies
+    
 
 element_of element [] = False
 element_of element (x:xs) = 
@@ -46,7 +58,7 @@ add_or_increment (k, v) (x:xs) =
 
 
 -- Returns an association list of characters and their frequency as integers. 
-count_char_frequency :: String -> [(Char, Integer)]
+count_char_frequency :: String -> [(Char, Int)]
 count_char_frequency str = count_char_frequency_helper str []
 count_char_frequency_helper [] acc = acc 
 count_char_frequency_helper (x:xs) acc = 
@@ -55,7 +67,7 @@ count_char_frequency_helper (x:xs) acc =
 
 
 -- Converts an association list to a list of leaves 
-make_leaves :: [(Char, Integer)] -> [(Tree)]
+make_leaves :: [(Char, Int)] -> [(Tree)]
 make_leaves lst = [(Leaf k v) | (k,v) <- lst]
 
 
@@ -69,9 +81,13 @@ sorted_insert_helper element (x:xs) acc =
     if element <= x then acc ++ [element, x] ++ xs 
     else sorted_insert_helper element xs (acc ++ [x])
 
+sorted_insert_many [] lst = lst 
+sorted_insert_many (x:xs) lst = 
+    sorted_insert_many xs (sorted_insert x lst)
 
-make_tree :: [Tree] -> Maybe Tree 
-make_tree leaf_list = 
+
+make_tree :: [Tree] -> Int -> Maybe Tree 
+make_tree leaf_list n = 
     -- First, sort the list. Do this only once.
     -- Until there are only two elements in the list, do the following: 
     -- 1. Take the two first elements of the sorted list (the two smallest ones) 
@@ -80,37 +96,39 @@ make_tree leaf_list =
     -- 4. Repeat.
     -- When there are only two elements left, combine them and return the resulting node.
     let sorted_leaf_list = sort leaf_list in 
-        make_tree_helper sorted_leaf_list
+        make_tree_helper sorted_leaf_list n
     
 -- TODO: Make trees maybe heheheheh
-make_tree_helper :: [Tree] -> Maybe Tree
-make_tree_helper [] = Nothing
-make_tree_helper [l1] = Just l1
-make_tree_helper (l1 : l2 : leaves) = 
-    let node = Node l1 l2 (sum_frequency l1 l2) in 
-        make_tree_helper (sorted_insert node leaves)
+make_tree_helper :: [Tree] -> Int -> Maybe Tree
+make_tree_helper [] n = Nothing
+make_tree_helper [l1] n = Just l1
+make_tree_helper tree_list n = 
+    let tree_nodes = take n tree_list
+        new_node = (Node tree_nodes (sum_frequencies tree_nodes))
+        remaining_list = set_minus tree_list tree_nodes in 
+        make_tree_helper (sorted_insert new_node remaining_list) n
 
 
 -- Use depth first to traverse the tree, adding 0's and 1's for every left and right 
 -- turn, respectively. When a leaf is encountered, add (letter, encoding) to the accumulator. 
-get_encoding_dict :: Maybe Tree -> [(Char, [Bit])]
+get_encoding_dict :: Maybe Tree -> [(Char, [Int])]
 get_encoding_dict tree = 
     case tree of 
         Nothing -> []
-        Just t -> get_encoding_dict_helper t [] []
+        Just t -> get_encoding_dict_helper t [] [] 0
 
-get_encoding_dict_helper :: Tree -> [Bit] -> [(Char, [Bit])] -> [(Char, [Bit])]
-get_encoding_dict_helper (Leaf letter _) encoding acc = 
+get_encoding_dict_helper :: Tree -> [Int] -> [(Char, [Int])] -> Int -> [(Char, [Int])]
+get_encoding_dict_helper (Leaf letter _) encoding acc n = 
     [(letter, encoding)] ++ acc 
 
-get_encoding_dict_helper (Node l1 l2 _) encoding acc = 
-    -- Left first
-    let left_acc = get_encoding_dict_helper l1 (encoding ++ [Zero]) acc in 
-        -- Then take the right
-        get_encoding_dict_helper l2 (encoding ++ [One]) left_acc
+get_encoding_dict_helper (Node (branch:branches) f) encoding acc n = 
+    let leftmost_acc = get_encoding_dict_helper branch (encoding ++ [n]) acc 0 in 
+        get_encoding_dict_helper (Node branches f) encoding (acc ++ leftmost_acc) (n + 1) 
+
+get_encoding_dict_helper (Node [] f) encoding acc n = acc
 
 
-get_encoded_message_from_dict :: String -> [(Char, [Bit])] -> [Bit]
+get_encoded_message_from_dict :: String -> [(Char, [Int])] -> [Int]
 get_encoded_message_from_dict [] encoding_dict = []
 get_encoded_message_from_dict [x] encoding_dict = 
     let encoding = lookup_value x encoding_dict in 
@@ -124,30 +142,30 @@ get_encoded_message_from_dict (x:xs) encoding_dict =
             Just enc -> enc ++ get_encoded_message_from_dict xs encoding_dict
             Nothing -> error "Could not find an encoding for the symbol: " x
 
-get_encoded_message :: String -> [Bit]
-get_encoded_message msg = 
+get_encoded_message :: String -> Int -> [Int]
+get_encoded_message msg n = 
     let freq = count_char_frequency msg 
         leaves = make_leaves freq 
-        tree = make_tree leaves 
+        tree = make_tree leaves n
         dict = get_encoding_dict tree in 
             get_encoded_message_from_dict msg dict 
 
 
-get_huffman_tree :: String -> Tree
-get_huffman_tree str = 
+get_huffman_tree :: String -> Int -> Tree
+get_huffman_tree str n = 
     let freq = count_char_frequency str 
-        tree = make_tree (make_leaves freq) in 
+        tree = make_tree (make_leaves freq) n in 
             case tree of 
                 Nothing -> error "Could not generate huffman tree for an empty message. " 
                 Just t -> t 
 
 
-decode_bit_stream :: [Bit] -> Tree -> String 
+decode_bit_stream :: [Int] -> Tree -> String 
 decode_bit_stream stream huffman_tree = traverse_tree_from_bit_stream stream huffman_tree huffman_tree ""
 
-some_tree = get_huffman_tree "hello"
+some_tree = get_huffman_tree "hello there" 5
 
-traverse_tree_from_bit_stream :: [Bit] -> Tree -> Tree -> String -> String
+traverse_tree_from_bit_stream :: [Int] -> Tree -> Tree -> String -> String
 traverse_tree_from_bit_stream [] huffman_tree root msg = 
     let Leaf letter _ = huffman_tree in 
             msg ++ [letter]
@@ -155,8 +173,7 @@ traverse_tree_from_bit_stream [] huffman_tree root msg =
 traverse_tree_from_bit_stream (x:xs) huffman_tree root msg =
     case huffman_tree of 
         -- Take the left node if encountering a 0, right node otherwise.
-        Node l1 l2 _ -> if x == Zero then traverse_tree_from_bit_stream xs l1 root msg
-                        else              traverse_tree_from_bit_stream xs l2 root msg
+        Node branches _ -> traverse_tree_from_bit_stream xs (branches!!x) root msg 
 
         -- If it's a leaf, add the corresponding letter to the message and start again from the root. 
         Leaf char _  -> let bit_stream = ([x] ++ xs) 
@@ -164,26 +181,26 @@ traverse_tree_from_bit_stream (x:xs) huffman_tree root msg =
                                 traverse_tree_from_bit_stream bit_stream root root decoded_message
 
                             
--- Simple converters
-bit_string_to_bit_arr :: String -> [Bit]
-bit_string_to_bit_arr str = [bit | c <- str, let bit = if c == '0' then Zero else One]
 
-
-bit_arr_to_bit_string :: [Bit] -> String
-bit_arr_to_bit_string arr = [char | b <- arr, let char = if b == Zero then '0' else '1']
 
 
 -- Run IO 
 run_encode = do 
     putStrLn "Please enter a message to encode: "
     user_input <- getLine 
+    putStrLn "... and the arity of the encoding alphabet: "
+    n_str <- getLine 
+    let n = read n_str :: Int
+    let encoded_message = get_encoded_message user_input n
+
     putStrLn "The encoded message is: "
-    let encoded_message = get_encoded_message user_input
     return encoded_message
 
 run_decode = do 
-    putStrLn "Please enter a message {0,1}* to decode with our proprietary Huffman tree: "
-    user_input <- getLine 
-    let decoded_message = decode_bit_stream (bit_string_to_bit_arr user_input) some_tree 
-    return decoded_message
-
+    putStrLn "Please enter an encoded message :: [Int] to decode: "
+    user_input <- getLine
+    let u_input = read user_input :: [Int]
+    putStrLn "Decoding message with proprietary and copyrighted n-ary Huffman tree..."
+    let msg = decode_bit_stream u_input some_tree
+    putStrLn "The decoded message is: "
+    return msg
